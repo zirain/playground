d20741c2045413290a23548024e74b93356ee1a2
zirain <zirain2009@gmail.com>
2020-03-03 21:20:28 +0800

    type alias

---
+++ go/helloworld/typealias.go
@@ -0,0 +1,18 @@
+package main
+import (
+    "fmt"
+)
+// 将NewInt定义为int类型
+type NewInt int
+// 将int取一个别名叫IntAlias
+type IntAlias = int
+func main() {
+    // 将a声明为NewInt类型
+    var a NewInt
+    // 查看a的类型名
+    fmt.Printf("a type: %T\n", a)
+    // 将a2声明为IntAlias类型
+    var a2 IntAlias
+    // 查看a2的类型名
+    fmt.Printf("a2 type: %T\n", a2)
+}
