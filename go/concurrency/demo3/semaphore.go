42c34323b996b9892e30b38c711fdcdfce17a2f9
zirain <zirain2009@hotmail.com>
2020-03-06 09:42:21 +0800

    semaphore

---
+++ go/concurrency/demo3/semaphore.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(4)
+	var wg sync.WaitGroup
+
+	sem := make(chan struct{}, 2) //最多允许2个并发同时执行
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+
+		go func(id int) {
+			defer wg.Done()
+
+			sem <- struct{}{}
+
+			defer func() {
+				<-sem
+			}()
+
+			time.Sleep(time.Second)
+			fmt.Println(id, time.Now())
+		}(i)
+	}
+
+	wg.Wait()
+}
