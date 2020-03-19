ddd6444f2a9a89cf8c439d82536610f4887bcfb0
he.jianpeng <he.jianpeng@oe.21vianet.com>
2020-03-19 10:27:31 +0800

    longest palindrome

--- go/leetcode/main.go
+++ go/leetcode/main.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 )
 
-func isRectangleOverlap(rec1 []int, rec2 []int) bool {
-	return !(rec1[2] <= rec2[0] || // left
-		rec1[3] <= rec2[1] || // bottom
-		rec1[0] >= rec2[2] || // right
-		rec1[1] >= rec2[3]) // top
+func longestPalindrome(s string) int {
+	count:=0
+	countDic := make(map[rune]int)
+	for _,chr := range s{
+		if _,ok:=countDic[chr]; ok{
+			countDic[chr]--
+			delete(countDic,chr)
+			count++
+		}else	{
+			countDic[chr] = 0
+		}
+	}
+
+	if len(countDic) > 0{
+		return count*2+1
+	}else{
+		return count*2
+	}
 }
 
 func main() {
-	fmt.Println(isRectangleOverlap([]int{0, 0, 2, 2}, []int{1, 1, 3, 3}))
-	fmt.Println(isRectangleOverlap([]int{0, 0, 1, 1}, []int{1, 0, 2, 1}))
+	fmt.Println(longestPalindrome("abccccdd"))
 }
