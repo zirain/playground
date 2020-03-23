0ba90da159599c0a01893cebe5bf4bbbf770c7de
zirain <zirain2009@hotmail.com>
2020-03-24 06:37:56 +0800

    the-masseuse-lcci

--- go/leetcode/main.go
+++ go/leetcode/main.go
@@ -1,21 +1,37 @@
 package main
 
-//Definition for singly-linked list. ListNode
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+import "fmt"
 
-func middleNode(head *ListNode) *ListNode {
-	slow := head
-	fast := head
-	for fast != nil && fast.Next != nil {
-		slow = slow.Next
-		fast = fast.Next.Next
+func massage(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
 	}
 
-	return slow
+	dp0 := 0
+	dp1 := nums[0]
+
+	for i := 1; i < n; i++ {
+		tdp0 := max(dp0, dp1)
+		tdp1 := dp0 + nums[i]
+
+		dp0 = tdp0
+		dp1 = tdp1
+	}
+
+	return max(dp0, dp1)
+}
+
+func max(x int, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
 }
 
 func main() {
+	fmt.Println(massage([]int{1, 2, 3, 1}))
+	fmt.Println(massage([]int{2, 7, 9, 3, 1}))
+	fmt.Println(massage([]int{2, 1, 4, 5, 3, 1, 1, 3}))
 }
