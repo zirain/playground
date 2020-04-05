91a50180d213d84fcc316b83ce5fbea04d016470
zirain <zirain2009@gmail.com>
2020-04-05 09:31:27 +0800

    LFUCache

--- go/leetcode/main.go
+++ go/leetcode/main.go
@@ -1,39 +1,115 @@
 package main
 
-import "fmt"
-
-func trap(height []int) int {
-	ans := 0
-	size := len(height)
-	for i := 1; i < size-1; i++ {
-		max_left, max_right := 0, 0
-		for j := i; j >= 0; j-- { //Search the left part for max bar size
-			max_left = max(max_left, height[j])
-		}
-		for j := i; j < size; j++ { //Search the right part for max bar size
-			max_right = max(max_right, height[j])
-		}
-		ans += min(max_left, max_right) - height[i]
-	}
-	return ans
+type LFUCache struct {
+	cache               map[int]*Node
+	freq                map[int]*DoubleLinkedList
+	ncap, size, minFreq int
 }
 
-func max(x int, y int) int {
-	if x > y {
-		return x
+func Constructor(capacity int) LFUCache {
+	return LFUCache{
+		cache: make(map[int]*Node),
+		freq:  make(map[int]*DoubleLinkedList),
+		ncap:  capacity,
 	}
-
-	return y
 }
 
-func min(x int, y int) int {
-	if x < y {
-		return x
+func (this *LFUCache) Get(key int) int {
+	if node, ok := this.cache[key]; ok {
+		this.IncFreq(node)
+		return node.val
 	}
-	return y
+	return -1
+}
+
+func (this *LFUCache) Put(key, value int) {
+	if this.ncap == 0 {
+		return
+	}
+	if node, ok := this.cache[key]; ok {
+		node.val = value
+		this.IncFreq(node)
+	} else {
+		if this.size >= this.ncap {
+			node := this.freq[this.minFreq].RemoveLast()
+			delete(this.cache, node.key)
+			this.size--
+		}
+		x := &Node{key: key, val: value, freq: 1}
+		this.cache[key] = x
+		if this.freq[1] == nil {
+			this.freq[1] = CreateDoubleLinkedList()
+		}
+		this.freq[1].AddFirst(x)
+		this.minFreq = 1
+		this.size++
+	}
+}
+
+func (this *LFUCache) IncFreq(node *Node) {
+	_freq := node.freq
+	this.freq[_freq].Remove(node)
+	if this.minFreq == _freq && this.freq[_freq].IsEmpty() {
+		this.minFreq++
+		delete(this.freq, _freq)
+	}
+
+	node.freq++
+	if this.freq[node.freq] == nil {
+		this.freq[node.freq] = CreateDoubleLinkedList()
+	}
+	this.freq[node.freq].AddFirst(node)
+}
+
+type DoubleLinkedList struct {
+	head, tail *Node
+}
+
+type Node struct {
+	prev, next     *Node
+	key, val, freq int
+}
+
+func CreateDoubleLinkedList() *DoubleLinkedList {
+	head, tail := &Node{}, &Node{}
+	head.next, tail.prev = tail, head
+	return &DoubleLinkedList{
+		head: head,
+		tail: tail,
+	}
+}
+
+func (this *DoubleLinkedList) AddFirst(node *Node) {
+	node.next = this.head.next
+	node.prev = this.head
+
+	this.head.next.prev = node
+	this.head.next = node
+}
+
+func (this *DoubleLinkedList) Remove(node *Node) {
+	node.prev.next = node.next
+	node.next.prev = node.prev
+
+	node.next = nil
+	node.prev = nil
+}
+
+func (this *DoubleLinkedList) RemoveLast() *Node {
+	if this.IsEmpty() {
+		return nil
+	}
+
+	last := this.tail.prev
+	this.Remove(last)
+
+	return last
+}
+
+func (this *DoubleLinkedList) IsEmpty() bool {
+	return this.head.next == this.tail
 }
 
 func main() {
-	height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
-	fmt.Println(trap(height))
+
 }
