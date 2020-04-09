a2d6723504df9c8d9336dd95378e980a6c014972
zirain <zirain2009@hotmail.com>
2020-04-09 08:15:28 +0800

    括号生成

--- go/leetcode/main.go
+++ go/leetcode/main.go
@@ -2,45 +2,31 @@ package main
 
 import "fmt"
 
-func get(x int) int {
-	res := 0
-	for x > 0 {
-		res += x % 10
-		x = x / 10
+func generateParenthesis(n int) []string {
+	output := make([]string, 0)
+	if n < 0 {
+		return output
 	}
-	return res
+
+	backtrace("", 0, 0, n, &output)
+	return output
 }
 
-func movingCount(m int, n int, k int) int {
-	if k == 0 {
-		return 1
+func backtrace(parenthesis string, lefts int, rights int, max int, output *[]string) {
+	if len(parenthesis) == (max * 2) {
+		*output = append(*output, parenthesis)
 	}
-	vis := make([][]int, m)
-	for i := 0; i < m; i++ {
-		vis[i] = make([]int, n)
-	}
-	ans := 1
-	vis[0][0] = 1
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			if (i == 0 && j == 0) || get(i)+get(j) > k {
-				continue
-			}
 
-			// 边界判断
-			if i-1 >= 0 {
-				vis[i][j] |= vis[i-1][j]
-			}
-			if j-1 >= 0 {
-				vis[i][j] |= vis[i][j-1]
-			}
-			ans += vis[i][j]
-		}
+	if lefts < max {
+		backtrace(parenthesis+"(", lefts+1, rights, max, output)
 	}
-	return ans
+
+	if rights < lefts {
+		backtrace(parenthesis+")", lefts, rights+1, max, output)
+	}
 }
 
 func main() {
-	fmt.Println(movingCount(2, 3, 1))
-	fmt.Println(movingCount(2, 3, 0))
+	fmt.Println(generateParenthesis(3))
+	fmt.Println(generateParenthesis(1))
 }
