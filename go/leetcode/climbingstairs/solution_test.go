afa3681fe047be7d8bae91cd3ec0605c3dde048d
zirain <zirain2009@hotmail.com>
2020-06-13 09:35:36 +0800

    爬楼梯

---
+++ go/leetcode/climbingstairs/solution_test.go
@@ -0,0 +1,13 @@
+package climbingstairs
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestClimbStairs(t *testing.T) {
+	fmt.Println(climbStairs(0))
+	fmt.Println(climbStairs(1))
+	fmt.Println(climbStairs(2))
+	fmt.Println(climbStairs(3))
+}
