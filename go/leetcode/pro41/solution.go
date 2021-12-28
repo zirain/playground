eb71bf2a2de928339e73ed9289eb634179ff9f54
hejianpeng <hejianpeng2@huawei.com>
2021-12-28 15:49:51 +0800

    leetcode pro41

--- go/leetcode/pro41/solution.go
+++ go/leetcode/pro41/solution.go
@@ -1,20 +1,19 @@
 package pro41
 
 func firstMissingPositive(nums []int) int {
-	
 	dict := make(map[int]bool, 0)
-	for _,val := range nums{
+	for _, val := range nums {
 		dict[val] = true
 	}
 
 	ans := 1
 	for {
-		if _,ok:=dict[ans];ok{
+		if _, ok := dict[ans]; ok {
 			ans++
-		}else{
+		} else {
 			break
 		}
 	}
 
-	return  ans
+	return ans
 }
