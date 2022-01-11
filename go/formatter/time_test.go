8dc21fbd42df71866e020f52de4ec87dfdd52f61
hejianpeng <hejianpeng2@huawei.com>
2022-01-11 16:22:21 +0800

    duration format

---
+++ go/formatter/time_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/bmizerany/assert"
+)
+
+func TestDuration(t *testing.T) {
+	cases := []struct {
+		d        time.Duration
+		expected string
+	}{
+		{
+			d:        time.Minute,
+			expected: "1m",
+		},
+	}
+	for _, tc := range cases {
+		t.Run("", func(t *testing.T) {
+			got := fmt.Sprintf("%d", tc.d)
+			assert.Equal(t, tc.expected, got)
+		})
+	}
+}
