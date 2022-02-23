5f73acbbda44fcfceae96d14d505dda04ab89e4d
zirain <zirain2009@gmail.com>
2022-02-24 00:37:12 +0800

    merge sorted list

---
+++ go/leetcode/pro21/solution.go
@@ -0,0 +1,19 @@
+package pro21
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	}
+
+	list2.Next = mergeTwoLists(list1, list2.Next)
+	return list2
+}
