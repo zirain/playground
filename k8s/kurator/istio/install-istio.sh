75a5515c6316b47262b83fecf57df6b4e5e21cb3
hejianpeng <hejianpeng2@huawei.com>
2022-10-10 20:01:07 +0800

    update install-istio.sh

--- k8s/kurator/istio/install-istio.sh
+++ k8s/kurator/istio/install-istio.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-LOGGING_LEVEL=debug kurator install istio --primary member1 --remote member2
+LOGGING_LEVEL=debug kurator install istio --primary member1 --remote member2 --set meshConfig.accessLogFile=/dev/stdout
