7e2e403be50ae071032c06b20df1506ade3f3f31
zirain <zirain2009@gmail.com>
2023-09-07 10:49:33 +0800

    update grafana

--- terraform/cloudsmith/main.tf
+++ terraform/cloudsmith/main.tf
@@ -1,7 +1,8 @@
+
 module "istio_monitoring_grafana" {
-  source  = "terraform.cloudsmith.io/terraform/istio-monitoring-grafana/tetrate"
-  version = "v0.0.1"
+  source  = "terraform.cloudsmith.io/tis-containers/istio-monitoring-grafana/tetrate"
+  version = "v0.1.0"
 
   gf_url  = "http://localhost:3000"
-  gf_auth = ""
+  gf_auth = "admin:admin" # default admin password for testing
 }
