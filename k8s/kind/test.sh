b5cf100a003ad8cd04b23ebcdeebfd0796d04d4a
zirain <zirain2009@gmail.com>
2024-07-26 16:29:15 +0800

    run with istio
    Signed-off-by: zirain <zirain2009@gmail.com>

--- k8s/kind/test.sh
+++ k8s/kind/test.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+kubectl label ns default istio-injection=enabled --overwrite
+istioctl install -y -f ./istio/iop/dual-stack.yaml
 kubectl apply -f ./k8s/kind/
 kubectl wait --for=condition=ready pod --all --timeout=300s
 kubectl get svc whoami -oyaml
