5ea893677734ceeb8b183e8d98388275afafd66b
zirain <zirain2009@gmail.com>
2024-07-26 16:46:23 +0800

    dump log

--- k8s/kind/test.sh
+++ k8s/kind/test.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+
+function dump_logs() {
+  kubectl logs -l app=whoami -c istio-init
+  kubectl logs -l app=sleep -c istio-init
+}
+
+trap dump_logs EXIT
+
 kubectl label ns default istio-injection=enabled --overwrite
 istioctl install -y -f ./istio/iop/dual-stack.yaml
 kubectl apply -f ./k8s/kind/
