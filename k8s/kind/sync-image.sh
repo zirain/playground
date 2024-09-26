35a9893e1b0aa3289a708b8cb81849429973e49d
zirain <zirain2009@gmail.com>
2024-09-26 23:32:34 +0800

    update sync image

--- k8s/kind/sync-image.sh
+++ k8s/kind/sync-image.sh
@@ -6,17 +6,21 @@ set -euo pipefail
 ISTIO_VERSION=${ISTIO_VERSION:-$(istioctl version --remote=false | cut -d ':' -f2 | xargs)}
 MIRROR_REGISTRY=${MIRROR_REGISTRY:-"192.168.4.146:5000"}
 
+# sync Istio images(distroless is used in Ambient)
 istioImages=(pilot proxyv2 ztunnel install-cni)
-
 for imageName in ${istioImages[@]} ; do
     crane cp "docker.io/istio/${imageName}:${ISTIO_VERSION}" "${MIRROR_REGISTRY}/istio/${imageName}:${ISTIO_VERSION}"
     crane cp "docker.io/istio/${imageName}:${ISTIO_VERSION}-distroless" "${MIRROR_REGISTRY}/istio/${imageName}:${ISTIO_VERSION}-distroless"
 done
 
-# MetalLB
-crane cp quay.io/metallb/controller:v0.13.10 "${MIRROR_REGISTRY}/metallb/controller:v0.13.10"
-crane cp quay.io/metallb/speaker:v0.13.10 "${MIRROR_REGISTRY}/metallb/speaker:v0.13.10"
+# sync image from docker.io
+images=(fluent/fluent-bit:2.1.4 kong/httpbin:latest curlimages/curl:latest grafana/grafana:11.0.0 prom/prometheus:v2.52.0 grafana/tempo:2.1.1 bats/bats:v1.4.1)
+for imageName in ${images[@]} ; do
+    crane cp "${imageName}" "${MIRROR_REGISTRY}/${imageName}"
+done
 
-# samples
-crane cp kong/httpbin:latest "${MIRROR_REGISTRY}/kong/httpbin:latest"
-crane cp curlimages/curl:latest "${MIRROR_REGISTRY}/curlimages/curl:latest"
+# sync images from quary.io
+queryImages=(prometheus-operator/prometheus-config-reloader:v0.73.2 metallb/controller:v0.13.10 metallb/speaker:v0.13.10)
+for imageName in ${queryImages[@]} ; do
+    crane cp "quay.io/${imageName}" "${MIRROR_REGISTRY}/${imageName}"
+done
