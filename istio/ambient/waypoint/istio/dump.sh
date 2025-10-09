749b2fded1c9afa198ecb30a324897fe301c9ff2
zirain <zirain2009@gmail.com>
2025-10-09 19:32:22 +0800

    update ambient demo

--- istio/ambient/waypoint/istio/dump.sh
+++ istio/ambient/waypoint/istio/dump.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 POD_NS=${POD_NS:-default}
+DEPLOYMENT=${DEPLOYMENT:-simple-http-waypoint}
 
-istioctl pc all -n $POD_NS deploy/simple-http-waypoint -o yaml > dumps/waypoint-all.yaml
-istioctl pc c -n $POD_NS deploy/simple-http-waypoint -o yaml > dumps/waypoint.cluster.yaml
-istioctl pc l -n $POD_NS deploy/simple-http-waypoint -o yaml > dumps/waypoint.listener.yaml
-istioctl pc r -n $POD_NS deploy/simple-http-waypoint -o yaml > dumps/waypoint.route.yaml
+istioctl pc all -n $POD_NS deploy/$DEPLOYMENT -o yaml > dumps/waypoint-all.yaml
+istioctl pc c -n $POD_NS deploy/$DEPLOYMENT -o yaml > dumps/waypoint.cluster.yaml
+istioctl pc l -n $POD_NS deploy/$DEPLOYMENT -o yaml > dumps/waypoint.listener.yaml
+istioctl pc r -n $POD_NS deploy/$DEPLOYMENT -o yaml > dumps/waypoint.route.yaml
 
 
-istioctl pc all -n $POD_NS deploy/simple-http-waypoint -o json > dumps/waypoint-all.json
-istioctl pc c -n $POD_NS deploy/simple-http-waypoint -o json > dumps/waypoint.cluster.json
-istioctl pc l -n $POD_NS deploy/simple-http-waypoint -o json > dumps/waypoint.listener.json
-istioctl pc r -n $POD_NS deploy/simple-http-waypoint -o json > dumps/waypoint.route.json
+istioctl pc all -n $POD_NS deploy/$DEPLOYMENT -o json > dumps/waypoint-all.json
+istioctl pc c -n $POD_NS deploy/$DEPLOYMENT -o json > dumps/waypoint.cluster.json
+istioctl pc l -n $POD_NS deploy/$DEPLOYMENT -o json > dumps/waypoint.listener.json
+istioctl pc r -n $POD_NS deploy/$DEPLOYMENT -o json > dumps/waypoint.route.json
