0cdb538d6a63e14c173c25d25054e93ae31b1918
zirain <zirain2009@gmail.com>
2026-09-04 11:02:43 +0800

    fix
    Signed-off-by: zirain <zirain2009@gmail.com>

--- k8s/kind/test.sh
+++ k8s/kind/test.sh
@@ -12,7 +12,7 @@ trap dump_logs EXIT
 
 kubectl label ns default istio-injection=enabled --overwrite
 istioctl install -y -f ./istio/iop/dual-stack.yaml
-kubectl apply -f ./k8s/kind/
+kubectl apply -f ./k8s/kind/testconfigs
 kubectl wait --for=condition=ready pod --all --timeout=300s
 kubectl get svc whoami -oyaml
 kubectl exec -it deploy/sleep -- curl -v whoami:80
